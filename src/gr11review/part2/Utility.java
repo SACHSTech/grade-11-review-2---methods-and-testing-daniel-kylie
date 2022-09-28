f20e9b99ea9d1ff576c879f9e59ae043d542b85b
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-09-27 20:44:31 -0400

    method 3 comments

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -8,21 +8,25 @@ public class Utility {
     }
 
     // Methods 3
+
     public static boolean xyzMiddle(String str){
+    
+        // when the string contains 'xyz' and length of string is odd
         if (str.contains("xyz") && (str.length() % 2 != 0)){
+
+            // create character placement expectations for 'xyz' in the middle
             int intYPlacement = (str.length() - 1) / 2;
             int intXPlacement = intYPlacement - 1;
             int intZPlacement = intZPlacement + 1;
 
+            // check if the character placements are in the middle
             if ((str.charAt(intYPlacement) == 'y') && (str.charAt(intXPlacement) == 'x') && (str.charAt(intZPlacement) == 'z')){
                 return true;
             }
             else{
                 return false;
-
             }
         }
-        
         else{
             return false;
         }
