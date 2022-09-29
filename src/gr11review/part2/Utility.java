c24d131d15a6fe20f2610281fa4696353f78bcb3
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-09-28 20:20:45 -0400

    Array 3 - Complete

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,14 +1,22 @@
 package gr11review.part2;
-import java.io.BufferedReader;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
 
-import java.io.IOException;
 
 
 public class Utility {
 
     public static void main (String[] args){
+
+        // Method 3 
         System.out.print(xyzMiddle("AAxyzB"));
+
+        // Array 3 - Checking the output
+        int myarray[] = {1, 0, 0, 1};
+        System.out.println(Arrays.toString(zeroFront(myarray)));
+
 
     }
 
@@ -47,9 +55,7 @@ public class Utility {
         return isXYZMiddle;
     }
 
-     
-    // File IO - Read 2
-    /*
+     /*
      * File IO - Read 2
      * Evaluates a list of words and determines which word will come first in alphabetical order
      * 
@@ -83,8 +89,44 @@ public class Utility {
 
     }
 
-    private static Reader FileReader(String filenametxt) {
-        return null;
+    /*
+     * Arrays 3 - One Dimensional 
+     * Returns an array that contains the same numbers as in the given array, except it is rearranged so that all the zeros are at the front of the array
+     * 
+     * @param nums the array 
+     * @return newArray, the new rearranged array with the zeros at the front and remaining integers following
+     * @author K. Sinclair
+     */
+
+    public static int[] zeroFront(int[] nums){
+        // define variable to count number of zeros and new array
+        int intZeroCount = 0;
+        int[] newArray = new int[nums.length];
+
+        // for loop to determine number of zeros and to put them at the front of the new array
+        for (int i = 0; i < nums.length; i++){
+
+            // if statement to check that the int is 0 and add the 0 at the front of the new array
+            if (nums[i] == 0){
+                newArray[intZeroCount] = 0;
+                intZeroCount++;
+            }
+        }
+        // for loop to return the rest of the int 
+        for (int j = 0; j < nums.length; j++){
+
+            // if statement to check that the int is not a 0 and add the int to the end of the new array
+            if (nums[j] != 0) {
+                newArray[intZeroCount++] = nums[j];
+            }
+        }
+        // return the new array with all the zeros in the front 
+        return newArray;
+
     }
+
+   
+
+    
 
 }
