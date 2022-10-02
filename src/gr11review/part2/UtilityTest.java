8a6548af7205a8686d53a76b06bc9ae6b71f3c39
DanielGu23 <daniel.gu23@ycdsbk12.ca>
2022-10-02 19:28:20 -0400

    File IO 1 Complete

--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -1,12 +1,16 @@
 package gr11review.part2;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
 import gr11review.part2.Utility;
 
 public class UtilityTest{
     
+    // Methods 1
     @Test
     public void xyBalanceTest1(){ 
         assertEquals(true, Utility.xyBalance("xxxy"));
@@ -21,5 +25,24 @@ public class UtilityTest{
     public void xyBalanceTest3(){
         assertEquals(true, Utility.xyBalance("xxxy2189das"));
     }
+
+    // IO reader 1
+    @Test
+    public void longestWordTest1(){
+        try{
+            assertEquals("consectetur", Utility.longestWord("words.txt"));
+        }catch(IOException e){       
+            System.out.println("File does not exist.");     
+        }
+    }
+    
+    @Test
+    public void longestWordTest2(){
+        try{
+            assertEquals("dasduio1212321332432", Utility.longestWord("words2.txt"));
+        }catch(IOException e){       
+            System.out.println("File does not exist.");     
+        }
+    } 
   
 }
